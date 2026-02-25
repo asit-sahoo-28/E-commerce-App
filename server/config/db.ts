6dfc360cecdcd0e88b251edc5334caa663ed8a79
Amit sahoo <sahooamit8989@gmail.com>
2026-02-25 06:44:01 +0530

    first commit

---
+++ server/config/db.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+
+const connectDB=async()=>{
+    mongoose.connection.on("connected",()=>{
+        console.log("MongoDB Connected")
+    })
+    await mongoose.connect(process.env.MONGO_URI as string)
+
+
+}
+
+export default connectDB;
